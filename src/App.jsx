e945554266f1a7626757f17b1e67bbb0d198aa6c
Hamzabaloch08 <hb.developer08@gmail.com>
2025-05-01 17:45:02 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "./components/button";
+import Button from "./components/Button";
 
 function App() {
   const [color, setColor] = useState("olive");
